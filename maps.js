c7472df97d9426eb5c083d5694279d885fd31b2f
danetempest <dabellstempest@gmail.com>
2026-01-13 11:44:24 -0800

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,19 +1,10 @@
-// maps.js - The Central Registry
+// maps.js
 const MAP_DATABASE = {
     "Oregon": {
-        "2F Kids/Dorms": "https://www.r6maps.com/img/oregon/oregon-2.jpg",
-        "1F Kitchen/Dining": "https://www.r6maps.com/img/oregon/oregon-1.jpg",
-        "Basement": "https://www.r6maps.com/img/oregon/oregon-0.jpg"
+        // Change "assets" to "maps" here
+        "2F": "maps/oregon/2f.jpg", 
+        "1F": "maps/oregon/1f.jpg",
+        "Basement": "maps/oregon/0f.jpg"
     },
-    "Clubhouse": {
-        "2F Gym/Bedroom": "https://www.r6maps.com/img/clubhouse/clubhouse-2.jpg",
-        "1F Bar/Stock": "https://www.r6maps.com/img/clubhouse/clubhouse-1.jpg",
-        "Basement": "https://www.r6maps.com/img/clubhouse/clubhouse-0.jpg"
-    },
-    "Consulate": {
-        "2F Admin": "https://www.r6maps.com/img/consulate/consulate-2.jpg"
-    }
+    // ... repeat for others
 };
-
-// NOTE: You can replace the URLs above with local file paths 
-// like "assets/oregon-2f.jpg" once you upload images to GitHub.
